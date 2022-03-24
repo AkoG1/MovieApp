36a50ade49807b79530f5c9237c2f66704968105
akogachechiladze <aleksandre.gachechiladze.1@btu.edu.ge>
2022-03-24 12:06:53 +0400

    1st commit (Added actorsPhotos on MovieDetailsFragment)

--- app/src/main/java/com/example/movieapp/ui/movieDetails/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/movieDetails/MovieDetailsFragment.kt
@@ -4,11 +4,13 @@ import android.util.Log
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movieapp.R
 import com.example.movieapp.databinding.MovieDetailsFragmentBinding
 import com.example.movieapp.extensions.setImage
 import com.example.movieapp.model.MovieDetailsModel
 import com.example.movieapp.ui.base.BaseFragment
+import com.example.movieapp.ui.movieDetails.adapter.ActorsAdapter
 import com.example.movieapp.ui.movieDetails.vm.MovieDetailsViewModel
 import com.example.movieapp.utils.Resource
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -20,10 +22,21 @@ class MovieDetailsFragment :
 
     private val safeArgs: MovieDetailsFragmentArgs by navArgs()
 
+    private lateinit var adapter: ActorsAdapter
+
     override fun init() {
+        initListeners()
+        initRecyclerView()
         getMovieDetails()
         observeWholeData()
-        initListeners()
+        observeActorsDetails()
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        adapter = ActorsAdapter()
+        binding.recyclerView.adapter = adapter
     }
 
     private fun initListeners() {
@@ -34,6 +47,10 @@ class MovieDetailsFragment :
 
     private fun getMovieDetails() {
         viewModel.getMovieDetails(id = safeArgs.id)
+    }
+
+    private fun getActorsDetails(actors: String) {
+        viewModel.getActorsDetails(actors = actors)
     }
 
     private fun observeWholeData() {
@@ -65,7 +82,7 @@ class MovieDetailsFragment :
             ratingTextView.text = it.rated
             description.text = it.plot
             coverIV.setImage(it.poster)
-            actors.text = it.actors
+            it.actors?.let { it1 -> getActorsDetails(it1) }
             Log.d("12345", "successObserve: ${it.actors}")
             if (!it.genre.isNullOrEmpty()) {
                 val genre = it.genre.split(",").map { it.trim() }
@@ -88,6 +105,25 @@ class MovieDetailsFragment :
             }
             progressBar.isVisible = false
         }
+    }
 
+    private fun observeActorsDetails() {
+        viewModel.actorsDetails.observe(viewLifecycleOwner) {
+            when (it) {
+                is Resource.Success -> {
+                    adapter.setData(it.data)
+                }
+                is Resource.Error -> {
+                    makeToastMessage(it.message!!)
+                    binding.progressBar.isVisible = false
+                }
+                is Resource.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+                else -> {
+                    makeToastMessage(getString(R.string.unknownError))
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/movieapp/ui/movieDetails/vm/MovieDetailsViewModel.kt
+++ app/src/main/java/com/example/movieapp/ui/movieDetails/vm/MovieDetailsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.movieapp.model.ActorsModel
 import com.example.movieapp.model.MovieDetailsModel
 import com.example.movieapp.repository.Repository
 import com.example.movieapp.utils.Resource
@@ -14,11 +15,21 @@ class MovieDetailsViewModel(private val repository: Repository) : ViewModel() {
     private val _movieDetails = MutableLiveData<Resource<MovieDetailsModel>>(Resource.Idle)
     val movieDetails: LiveData<Resource<MovieDetailsModel>> get() = _movieDetails
 
+    private val _actorsDetails = MutableLiveData<Resource<List<ActorsModel>>>(Resource.Idle)
+    val actorsDetails: LiveData<Resource<List<ActorsModel>>> get() = _actorsDetails
+
 
     fun getMovieDetails(id: String) {
         viewModelScope.launch {
             _movieDetails.value = Resource.Loading
             _movieDetails.value = repository.getMovieDetails(id = id)
+        }
+    }
+
+    fun getActorsDetails(actors: String) {
+        viewModelScope.launch {
+            _actorsDetails.value = Resource.Loading
+            _actorsDetails.value = repository.getActorsDetails(actors = actors)
         }
     }
 }
